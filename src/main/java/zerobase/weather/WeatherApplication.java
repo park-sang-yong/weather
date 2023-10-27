10833776cb69d9962a40518a9248e2dd9001cd0f
parksangyong <qkr152369@gmail.com>
2023-10-27 13:04:43 +0900

    로깅 설정완료(사용 코드 추가 요구)

--- src/main/java/zerobase/weather/WeatherApplication.java
+++ src/main/java/zerobase/weather/WeatherApplication.java
@@ -2,11 +2,13 @@ package zerobase.weather;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootApplication
 @EnableTransactionManagement
+@EnableScheduling
 public class WeatherApplication {
 
 	public static void main(String[] args) {
